659690b02a1c9a5bf9e1b500a0df2f9b3c62cf84
Rikizhi <hamzahnuraria@gmail.com>
2023-05-30 20:55:05 +0700

    modified:   about.html 	modified:   contact.html 	modified:   css/style.css 	modified:   index.html 	modified:   js/script.js 	modified:   menu.html

--- js/script.js
+++ js/script.js
@@ -1,16 +1,29 @@
-// Toggle class active
-const navbarNav = document.querySelector('.navbar-nav');
+// Toggle class active hamburger menu
+const navbarNav = document.querySelector(".navbar-nav");
 // ketika Hamburger Menu di Klik
-document.querySelector('#hamburger-menu').onclick = () => {
-     navbarNav.classList.toggle('active');
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
 };
 
-// Klik luar sidebar untuk menghilangkan nav
-const hamburger = document.querySelector('#hamburger-menu');
+// Toggle class active search form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
 
-document.addEventListener('click', function(e) {
-     if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
-          navbarNav.classList.remove('active');
-     }
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
+// Klik di luar elemen hamburger & search button
+const hm = document.querySelector("#hamburger-menu");
+const sb = document.querySelector("#search-button");
+
+document.addEventListener("click", function (e) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
+  }
 });
-
