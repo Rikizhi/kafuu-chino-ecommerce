9f05a391cb35b291db0ce71f2fa21aed8cb7f75a
Rikizhi <hamzahnuraria@gmail.com>
2023-06-21 23:06:15 +0700

    modified:   css/style.css 	modified:   js/script.js

--- js/script.js
+++ js/script.js
@@ -41,5 +41,27 @@ document.addEventListener("click", function (e) {
 });
 
 // Modal Box
+const itemDetailModal = document.querySelector('#item-detail-modal');
+const itemDetailButtons = document.querySelectorAll('.item-detail-button');
+
+itemDetailButtons.forEach((btn) => {
+  btn.onclick = (e) => {
+    itemDetailModal.style.display = 'flex';
+    e.preventDefault();
+  };
+})
 
 
+// klik tombol close
+document.querySelector('.modal .close-icon').onclick = (e) => {
+  itemDetailModal.style.display = 'none';
+  e.preventDefault();
+};
+
+// klik di luar modal
+
+window.onclick = (e) => {
+  if (e.target === itemDetailModal) {
+    itemDetailModal.style.display = 'none';
+  }
+};
